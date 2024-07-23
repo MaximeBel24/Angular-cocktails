99eb2d978f3b425e0fec8dad8c9d8279dee04f09
Maxime <maxime.b2494@gmail.com>
2024-07-23 23:18:30 +0200

    liste de cocktail + details d'un cocktail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,18 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
+import { Cocktail } from './interfaces/cocktail.interface';
+import { CocktailListComponent } from "./cocktail-list/cocktail-list.component";
+import { CocktailDetailsComponent } from "./cocktail-details/cocktail-details.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, CocktailListComponent, CocktailDetailsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'cocktails';
+
 }
